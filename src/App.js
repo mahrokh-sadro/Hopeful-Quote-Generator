6c282637f9bf1989e3759c2dc5db3dc8e851f605
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-21 13:58:52 -0400

    implementing the function to fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 
 
 import React from 'react';
+import axios from 'axios';
+
 
 import './App.css';
 
-class App extends React.Component{
-    render(){
-        return(
-            App
+class App extends React.Component {
+    state = { advice: '' };
+    componentDidMount() {
+        this.fetchingAPI();
+    }
+
+    fetchingAPI = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then(res => {
+                console.log(res.data.slip.advice);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+    render() {
+        return (
+            <h1>this.fetchingAPI(); </h1>
         )
     }
 }
