f16f3d1222ff0598fbbcf98e2d7658e0e3a1777c
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-23 21:49:46 -0400

    fixing the button

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ class App extends React.Component {
             <div className="app">
                 <div className="card">
                     <h1 className="heading">{advice}</h1>
-                    <div className="button" onClick={this.fetchingAPI}>
+                    <button className="button" onClick={this.fetchingAPI}>
                         <span>Advise Me!</span>
-                    </div>
+                    </button>
                 </div>
             </div>
         );
